050dfd5c27d2f58dc5f66570b8455561c640bc1f
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-11-30 17:14:29 -0300

    fix: Ajuste geral . falta arrumar o slider

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Daimu-Website
+
 https://stitch.withgoogle.com/u/0/projects/6151531901648507219?pli=1
 https://stitch.withgoogle.com/projects/65310473141875012
 
-melhorR FOOTER
-    acesso rapido
-    redes sociais
+melhorR FOOTER x
+acesso rapido
+redes sociais
 
-formulario em 2 colunas para tela grande
+formulario em 2 colunas para tela grande x
 
 adicionar os dois menus /(modal/paginas)
 
 botar masi efeito js
 
-slider na galeria desktop
+slider na galeria desktop x
--- aplication/js/script.js
+++ aplication/js/script.js
@@ -1,82 +1,73 @@
 $(document).ready(function () {
-    $('#hamburguer').on('click', function () {
+  $("#hamburguer").on("click", function () {
+    $(".nav-menu").toggleClass("is-open");
 
-        $('.nav-menu').toggleClass('is-open');
+    $(this).toggleClass("is-open");
+  });
 
-        $(this).toggleClass('is-open');
-    });
+  $(".nav-menu a").on("click", function () {
+    if ($(".nav-menu").hasClass("is-open")) {
+      $(".nav-menu").removeClass("is-open");
+      $("#hamburguer").removeClass("is-open");
+    }
+  });
 
-    $('.nav-menu a').on('click', function () {
-        if ($('.nav-menu').hasClass('is-open')) {
-            $('.nav-menu').removeClass('is-open');
-            $('#hamburguer').removeClass('is-open');
-        }
-    });
+  $(".titulos .tab-title").on("click", function () {
+    const $clickedTitle = $(this);
+    const targetId = $clickedTitle.data("target");
+    const $targetContent = $(targetId);
 
-
-    $('.titulos .tab-title').on('click', function () {
-        const $clickedTitle = $(this);
-        const targetId = $clickedTitle.data('target');
-        const $targetContent = $(targetId);
-
-        if ($clickedTitle.hasClass('titulo-ativo')) {
-            return;
-        }
-
-        $('.titulos .tab-title').removeClass('titulo-ativo');
-        $clickedTitle.addClass('titulo-ativo');
-
-        $('.espacos-evento .description').not(targetId).stop(true, true).fadeOut(500);
-
-        $targetContent.stop(true, true).css('display', 'flex').hide().fadeIn(500);
-    });
-
-
-    $('.omakase-text').trigger('click');
-
-    $(".fotos.owl-carousel").owlCarousel({
-        margin: 10,
-        loop: true,
-        nav: false,
-        dots: true,
-        autoplay: false,
-        autoplayTimeout: 5000,
-        touchDrag: true,
-        mouseDrag: true,
-        infinite: false,
-
-        responsive: {
-            0: {
-                items: 1.1,
-                stagePadding: 10,
-                dots: false,
-
-            },
-            768: {
-                items: 1,
-
-            },
-            1200: {
-                items: 1,
-            }
-        }
-    });
-});
-
-var hasClicked = true
-
-$('.mapa-button').on('click', function () {
-    const $iframe = $(document.getElementsByClassName('mapa'));
-
-    if ($(window).width() <= 768) {
-
-        if (hasClicked) {
-            $iframe.css('display', '').fadeIn(1000);
-            hasClicked = false
-        } else {
-            $iframe.css('display', 'none').hide().fadeOut(500);
-            hasClicked = true
-        }
+    if ($clickedTitle.hasClass("titulo-ativo")) {
+      return;
     }
 
-})
+    $(".titulos .tab-title").removeClass("titulo-ativo");
+    $clickedTitle.addClass("titulo-ativo");
+
+    $(".espacos-evento .description")
+      .not(targetId)
+      .stop(true, true)
+      .fadeOut(500);
+
+    $targetContent.stop(true, true).css("display", "flex").hide().fadeIn(500);
+  });
+
+  $(".omakase-text").trigger("click");
+
+  $(".fotos.owl-carousel").owlCarousel({
+    margin: 10 /* Espaço horizontal entre as colunas */,
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: false,
+
+    responsive: {
+      0: {
+        items: 1.2 /* Mobile: Mostra 1 coluna (2 fotos empilhadas) + pedaço da proxima */,
+        stagePadding: 5,
+      },
+      768: {
+        items: 2 /* Tablet: 2 colunas (4 fotos visíveis) */,
+      },
+      1024: {
+        items: 4 /* Desktop: 4 colunas x 2 linhas (8 fotos visíveis!) */,
+      },
+    },
+  });
+});
+
+var hasClicked = true;
+
+$(".mapa-button").on("click", function () {
+  const $iframe = $(document.getElementsByClassName("mapa"));
+
+  if ($(window).width() <= 768) {
+    if (hasClicked) {
+      $iframe.css("display", "").fadeIn(1000);
+      hasClicked = false;
+    } else {
+      $iframe.css("display", "none").hide().fadeOut(500);
+      hasClicked = true;
+    }
+  }
+});
