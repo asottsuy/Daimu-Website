5e98ebb218385f846922c7d1c248f80709324d3d
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-11-30 22:45:43 -0300

    Trabalho finalizado

--- aplication/js/script.js
+++ aplication/js/script.js
@@ -39,18 +39,16 @@ $(document).ready(function () {
     loop: true,
     nav: false,
     dots: true,
-    autoplay: false,
+    autoplay: true,
+    speed: 2000,
 
     responsive: {
       0: {
         items: 1.2 /* Mobile: Mostra 1 coluna (2 fotos empilhadas) + pedaço da proxima */,
         stagePadding: 5,
       },
-      768: {
-        items: 2 /* Tablet: 2 colunas (4 fotos visíveis) */,
-      },
       1024: {
-        items: 4 /* Desktop: 4 colunas x 2 linhas (8 fotos visíveis!) */,
+        items: 5 /* Desktop: 4 colunas x 2 linhas (8 fotos visíveis!) */,
       },
     },
   });
@@ -70,4 +68,35 @@ $(".mapa-button").on("click", function () {
       hasClicked = true;
     }
   }
+});
+
+// Inicializando o ScrollReveal com configurações padrão
+const sr = ScrollReveal({
+  origin: "bottom", // De onde o elemento vem (baixo, cima, esq, dir)
+  distance: "50px", // Distância que ele percorre
+  duration: 1000, // Duração em ms (1 segundo)
+  delay: 200, // Atraso antes de começar
+  reset: false, // Se 'true', anima toda vez que rolar. 'false' anima só uma vez.
+});
+
+/* --- APLICANDO AS ANIMAÇÕES --- */
+
+// 1. Títulos e Textos Principais (Aparecem de baixo para cima)
+sr.reveal(".home-text, .titulo, .section-title");
+
+// 2. Imagens de Destaque (Vêm da esquerda)
+sr.reveal(".about-img, .left-image", {
+  origin: "left",
+});
+
+// 3. Textos de Descrição (Vêm da direita)
+sr.reveal(".about-text, .right-text", {
+  origin: "right",
+  delay: 400,
+});
+
+sr.reveal("#home, .our-story, .img-sr", {
+  delay: 400,
+  distance: "30px",
+  duration: 1000,
 });
